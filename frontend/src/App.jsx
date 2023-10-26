3ff2c70c40b66ad4053216baea57fc3f4191eebf
raph-bard <raphael.bard.pro@googlemail.com>
2023-10-26 17:35:10 +0200

    Style movie page and add VPN info

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -14,7 +14,29 @@ function toHoursAndMinutes(totalMinutes) {
   const minutes = totalMinutes % 60;
   const hours = Math.floor(totalMinutes / 60);
 
-  return `${hours}h${minutes > 0 ? ` ${minutes}m` : ""}`;
+  return `${hours}h${minutes > 0 ? `${minutes}` : ""}`;
+}
+
+function getFlagEmoji(countryCode) {
+  const codePoints = countryCode
+    .toUpperCase()
+    .split("")
+    .map((char) => 127397 + char.charCodeAt());
+  return String.fromCodePoint(...codePoints);
+}
+
+function getProviderCountries(details, providerId) {
+  const providers = details["watch/providers"].results;
+  const result = [];
+  for (const country in providers) {
+    if (
+      providers[country].flatrate &&
+      providers[country].flatrate.find((p) => p.provider_id === providerId)
+    ) {
+      result.push(getFlagEmoji(country));
+    }
+  }
+  return result;
 }
 
 function App() {
@@ -24,7 +46,7 @@ function App() {
     const fetchData = async () => {
       try {
         const response = await fetch(
-          "https://api.themoviedb.org/3/movie/575264?language=fr-FR&append_to_response=videos,credits,watch/providers",
+          "https://api.themoviedb.org/3/movie/238?language=fr-FR&append_to_response=videos,credits,watch/providers",
           options
         );
         if (response.ok) {
@@ -45,7 +67,7 @@ function App() {
     <div className="App">
       {movieDetails && (
         <div>
-          <div className="container-max">
+          <div className="container-max mb-20">
             <div
               className="backdrop"
               style={{
@@ -58,61 +80,90 @@ function App() {
             src={`https://image.tmdb.org/t/p/w500/${movieDetails.poster_path}`}
             alt={movieDetails.title}
           /> */}
-            <h1>{movieDetails.title}</h1>
-            <ul>
+            <h1 className="mb-20">{movieDetails.title}</h1>
+            <ul className="infos-list mb-30">
               <li>{movieDetails.release_date.slice(0, 4)}</li>
               <li>{toHoursAndMinutes(movieDetails.runtime)}</li>
               <li>
-                <ul>
-                  {movieDetails.genres.map((genre) => (
-                    <li key={genre.id}>{genre.name}</li>
-                  ))}
-                </ul>
+                {movieDetails.genres.map((objet) => objet.name).join(", ")}
               </li>
               <li>
+                {"Réalisateur: "}
                 {
                   movieDetails.credits.crew.find(
                     (person) => person.job === "Director"
                   ).name
                 }
               </li>
-              <li>{movieDetails.vote_average.toFixed(1)}</li>
+              <li className="note">{movieDetails.vote_average.toFixed(1)}</li>
             </ul>
-            <h2>Synopsis</h2>
-            <p>{movieDetails.overview}</p>
-            {/* <iframe
-              width="853"
-              height="480"
-              src={`https://www.youtube.com/embed/${movieDetails.videos.results[0].key}`}
-              frameBorder="0"
-              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-              allowFullScreen
-              title="Embedded youtube"
-            /> */}
-            <ul>
-              {movieDetails.credits.cast.slice(0, 5).map((person) => (
-                <li key={person.id}>
-                  {person.name}
-                  <img
-                    src={`https://image.tmdb.org/t/p/w500/${person.profile_path}`}
-                    alt={person.name}
-                  />
-                </li>
-              ))}
-            </ul>
-            <ul>
+            <h2 className="mb-20">Synopsis</h2>
+            <p className="mb-30">{movieDetails.overview}</p>
+            <h2 className="mb-20">Plateformes de streaming</h2>
+            <ul className="platform-list dflex mb-40">
               {movieDetails["watch/providers"].results.FR &&
                 movieDetails["watch/providers"].results.FR.flatrate.map(
                   (platform) => (
                     <li key={platform.provider_id}>
-                      {platform.provider_name}
                       <img
+                        className="mb-10"
                         src={`https://image.tmdb.org/t/p/w300/${platform.logo_path}`}
                         alt={platform.provider_name}
                       />
                     </li>
                   )
                 )}
+            </ul>
+            {console.info(getProviderCountries(movieDetails, 8))}
+            <h3>Netflix</h3>
+            <ul className="horizontal-list">
+              {getProviderCountries(movieDetails, 8).map((country) => {
+                return <li>{country}</li>;
+              })}
+            </ul>
+            <h3>Amazon Prime Video</h3>
+            <ul className="horizontal-list">
+              {getProviderCountries(movieDetails, 119).map((country) => {
+                return <li>{country}</li>;
+              })}
+            </ul>
+            <h3>Disney Plus</h3>
+            <ul className="horizontal-list">
+              {getProviderCountries(movieDetails, 337).map((country) => {
+                return <li>{country}</li>;
+              })}
+            </ul>
+            <h2 className="mb-20">Bande-annonce</h2>
+          </div>
+          <div className="container-max mb-40">
+            <div className="dflex-center">
+              <iframe
+                width="100%"
+                height="auto"
+                src={`https://www.youtube.com/embed/${movieDetails.videos.results[0].key}`}
+                frameBorder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                allowFullScreen
+                title="Embedded youtube"
+              />
+            </div>
+          </div>
+          <div className="container">
+            <h2 className="mb-20">Casting</h2>
+            <ul className="horizontal-list dflex mb-40">
+              {movieDetails.credits.cast.slice(0, 10).map((person) => (
+                <li className="t-center" key={person.id}>
+                  <figure className="mb-20">
+                    <img
+                      className="mb-10"
+                      x
+                      src={`https://image.tmdb.org/t/p/w500/${person.profile_path}`}
+                      alt={person.name}
+                    />
+                    <figcaption>{person.name}</figcaption>
+                  </figure>
+                </li>
+              ))}
             </ul>
           </div>
         </div>
