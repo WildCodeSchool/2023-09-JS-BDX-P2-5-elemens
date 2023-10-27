a0e7b19cd54310f57a12afff5b0120e1ba7e95d8
bfsylvain <sylvainbonnaure@hotmail.com>
2023-10-27 13:53:08 +0200

    movie cards update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -51,15 +51,44 @@ function App() {
               width: "220px",
             }}
           >
-            {movie.poster_path && (
-              <img
-                id={movie.id}
-                style={{ width: "200px", margin: "10px 10px 0 10px" }}
-                src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
-                alt="movie-poster"
-              />
-            )}
-            <div>
+            <div
+              className="img-container"
+              style={{
+                width: "220px",
+                height: "365px",
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+            >
+              {movie.poster_path ? (
+                <img
+                  id={movie.id}
+                  style={{ width: "200px", margin: "10px 10px 0 10px" }}
+                  src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+                  alt="movie-poster"
+                />
+              ) : (
+                <div
+                  style={{
+                    display: "flex",
+                    justifyContent: "center",
+                    alignItems: "center",
+                    backgroundColor: "#202124",
+                    width: "200px",
+                    height: "300px",
+                    margin: "10px 10px 0 10px",
+                  }}
+                >
+                  <img
+                    style={{ width: "120px" }}
+                    src="src/assets/pictures/logo_elemen5.png"
+                    alt="logo elemen5"
+                  />
+                </div>
+              )}
+            </div>
+            <div style={{ height: "3rem" }}>
               <p id={movie.id} style={{ textAlign: "center", marginTop: "0" }}>
                 {movie.title}
               </p>
