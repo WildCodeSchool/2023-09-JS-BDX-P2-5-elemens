c416a52ef4942f970997a8f0bdfd4dc47c7eef8c
bfsylvain <sylvainbonnaure@hotmail.com>
2023-10-29 17:15:09 +0100

    mainResearch component created

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,15 +3,22 @@ import axios from "axios";
 import Headroom from "react-headroom";
 import "./App.css";
 import Navbar from "./components/Navbar";
+import MainResearch from "./components/MainResearch";
 
 function App() {
   const [textFound, setTextFound] = useState("");
   const [movieList, setMovieList] = useState([]);
+  const [pageNumber, setPageNumber] = useState(1);
 
   const options = {
     method: "GET",
     url: "https://api.themoviedb.org/3/search/movie",
-    params: { query: `${textFound}`, include_adult: "true", language: "fr" },
+    params: {
+      query: `${textFound}`,
+      include_adult: "true",
+      language: "fr",
+      page: `${pageNumber}`,
+    },
     headers: {
       accept: "application/json",
       Authorization:
@@ -36,65 +43,11 @@ function App() {
   return (
     <div>
       <Headroom>
-        <Navbar setTextFound={setTextFound} />
+        <Navbar setTextFound={setTextFound} setPageNumber={setPageNumber} />
       </Headroom>
-      <div
-        style={{ display: "flex", flexWrap: "wrap", justifyContent: "center" }}
-      >
-        {movieList.map((movie) => (
-          <div
-            style={{
-              display: "flex",
-              flexDirection: "column",
-              alignItems: "center",
-              justifyContent: "center",
-              width: "220px",
-            }}
-          >
-            <div
-              className="img-container"
-              style={{
-                width: "220px",
-                height: "365px",
-                display: "flex",
-                justifyContent: "center",
-                alignItems: "center",
-              }}
-            >
-              {movie.poster_path ? (
-                <img
-                  id={movie.id}
-                  style={{ width: "200px", margin: "10px 10px 0 10px" }}
-                  src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
-                  alt="movie-poster"
-                />
-              ) : (
-                <div
-                  style={{
-                    display: "flex",
-                    justifyContent: "center",
-                    alignItems: "center",
-                    backgroundColor: "#202124",
-                    width: "200px",
-                    height: "300px",
-                    margin: "10px 10px 0 10px",
-                  }}
-                >
-                  <img
-                    style={{ width: "120px" }}
-                    src="src/assets/pictures/logo_elemen5.png"
-                    alt="logo elemen5"
-                  />
-                </div>
-              )}
-            </div>
-            <div style={{ height: "3rem" }}>
-              <p id={movie.id} style={{ textAlign: "center", marginTop: "0" }}>
-                {movie.title}
-              </p>
-            </div>
-          </div>
-        ))}
+
+      <div className="maim-area">
+        <MainResearch movieList={movieList} />
       </div>
     </div>
   );
