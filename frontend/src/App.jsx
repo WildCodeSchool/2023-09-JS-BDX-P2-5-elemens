121f85efb0ec5740becc94845fe9439a63d506d6
raph-bard <raphael.bard.pro@googlemail.com>
2023-10-30 11:21:28 +0100

    fin du design page detail-films en mobile

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -45,6 +45,11 @@ function getProviderCountries(details, providerId) {
 function App() {
   const [movieDetails, setMovieDetails] = useState(null);
   const [vpn, setVpn] = useState(false);
+  const [trailerPopup, setTrailerPopup] = useState(false);
+
+  const toggleTrailerPopup = () => {
+    setTrailerPopup(!trailerPopup);
+  };
 
   useEffect(() => {
     const fetchData = async () => {
@@ -53,6 +58,10 @@ function App() {
           "https://api.themoviedb.org/3/movie/550?language=fr-FR&append_to_response=videos,credits,watch/providers",
           options
         );
+        // fight club : 550
+        // mission impossible : 575264
+        // usual suspects : 629
+        // astérix : 2899
         if (response.ok) {
           const data = await response.json();
           setMovieDetails(data);
@@ -68,10 +77,10 @@ function App() {
   }, []);
 
   return (
-    <div className="App">
+    <div className={`App${trailerPopup === true ? " no-scroll" : ""}`}>
       {movieDetails && (
-        <div>
-          <div className="container-max mb-20">
+        <div className="movie-detail">
+          <div className="container-max">
             <div
               className="backdrop"
               style={{
@@ -84,26 +93,39 @@ function App() {
             src={`https://image.tmdb.org/t/p/w500/${movieDetails.poster_path}`}
             alt={movieDetails.title}
           /> */}
-            <h1 className="mb-20 t-center">{movieDetails.title}</h1>
-            <ul className="infos-list mb-30">
-              <li>{movieDetails.release_date.slice(0, 4)}</li>
-              <li>{toHoursAndMinutes(movieDetails.runtime)}</li>
-              <li>
+            <div className="movies-infos-container mb-30">
+              <h1 className="mb-20 t-center">{movieDetails.title}</h1>
+              <i className="note">{movieDetails.vote_average.toFixed(1)}</i>
+              <div className="infos-note dflex-center mb-20">
+                <div>
+                  <button
+                    type="button"
+                    className="blue-button icon-play"
+                    onClick={toggleTrailerPopup}
+                  >
+                    Bande annonce
+                  </button>
+                </div>
+              </div>
+              <p className="t-center mb-20">
+                {movieDetails.release_date.slice(0, 4)}
+                <b>•</b>
+                {toHoursAndMinutes(movieDetails.runtime)}
+                <b>•</b>
                 {movieDetails.genres.map((objet) => objet.name).join(", ")}
-              </li>
-              <li>
-                {"Réalisateur: "}
+                <br />
+                <i>Réalisé par </i>
                 {
                   movieDetails.credits.crew.find(
                     (person) => person.job === "Director"
                   ).name
                 }
-              </li>
-              <li className="note">{movieDetails.vote_average.toFixed(1)}</li>
-            </ul>
-            <h2 className="mb-20">Synopsis</h2>
-            <p className="mb-30">{movieDetails.overview}</p>
-            <h2 className="mb-20">Plateformes de streaming</h2>
+              </p>
+            </div>
+            <h2 className="mb-20 blue-title">Synopsis</h2>
+            <p className="tagline mb-20">"{movieDetails.tagline}"</p>
+            <p className="mb-60">{movieDetails.overview}</p>
+            <h2 className="mb-20 blue-title">Plateformes de streaming</h2>
             <div className="switch-button">
               <button
                 type="button"
@@ -185,23 +207,7 @@ function App() {
             </div>
           </div>
           <div className="container">
-            <h2 className="mb-20">Bande-annonce</h2>
-          </div>
-          <div className="container-max mb-40">
-            <div className="dflex-center">
-              <iframe
-                width="100%"
-                height="auto"
-                src={`https://www.youtube.com/embed/${movieDetails.videos.results[0].key}`}
-                frameBorder="0"
-                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                allowFullScreen
-                title="Embedded youtube"
-              />
-            </div>
-          </div>
-          <div className="container">
-            <h2 className="mb-20">Casting</h2>
+            <h2 className="mb-20 blue-title">Têtes d'affiche</h2>
             <ul className="horizontal-list mb-40">
               {movieDetails.credits.cast.slice(0, 10).map((person) => (
                 <li className="t-center" key={person.id}>
@@ -216,6 +222,30 @@ function App() {
                 </li>
               ))}
             </ul>
+          </div>
+          <div
+            className={`dflex-center popup-container${
+              trailerPopup === true ? " active" : ""
+            }`}
+          >
+            <div className="trailer-popup">
+              <iframe
+                width="100%"
+                height="auto"
+                src={`https://www.youtube.com/embed/${movieDetails.videos.results[0].key}`}
+                frameBorder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                allowFullScreen
+                title="Embedded youtube"
+              />
+              <button
+                type="button"
+                className="close-popup"
+                onClick={toggleTrailerPopup}
+              >
+                Test
+              </button>
+            </div>
           </div>
         </div>
       )}
